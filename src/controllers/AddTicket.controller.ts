53746119ad12c60d65a6b502889702985fd588da
onfranciis <onukwuf@gmail.com>
2024-01-14 11:25:59 +0100

    feat: create AddTicket controller

---
+++ src/controllers/AddTicket.controller.ts
@@ -0,0 +1,35 @@
+import { RequestHandler } from "express";
+import Ticket from "../model/Ticket.model";
+import { TicketValidator } from "../utils/TicketValidator";
+import { ITicket } from "../types/Model.types";
+
+const AddTicket: RequestHandler = async (req, res) => {
+  const form = req.body as ITicket;
+
+  await TicketValidator(form)
+    .then(async () => {
+      const { name: Oname, phoneNumber: OphoneNumber, item: Oitem } = form;
+
+      try {
+        const addedTicket = await Ticket.create({
+          name: Oname,
+          phoneNumber: OphoneNumber,
+          date: Date.now(),
+          item: Oitem,
+        });
+
+        const { name, phoneNumber, item, date } = addedTicket;
+
+        res.send({ error: null, result: { name, phoneNumber, item, date } });
+      } catch (err) {
+        console.log(err);
+        res.status(500).send({ error: `Something went wrong`, result: null });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send({ ...err });
+    });
+};
+
+export default AddTicket;
