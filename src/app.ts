2aee9512192be3f81630af38a24649948587dc55
onfranciis <onukwuf@gmail.com>
2024-01-14 11:26:11 +0100

    feat: add /donate route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import AddTicket from "./controllers/AddTicket.controller";
+import { json } from "body-parser";
 
 export const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send({ err: null, result: "Connected" });
 });
+
+app.post("/donate", AddTicket);
 
 app.all("*", (req, res) => {
   res.status(404).send({ err: "Route not found", result: null });
