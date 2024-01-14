2bd76272eb8b92e89091a31b7a0a1dc1f4374a06
onfranciis <onukwuf@gmail.com>
2024-01-14 11:25:22 +0100

    feat: create TicketValidator

---
+++ src/utils/TicketValidator.ts
@@ -0,0 +1,33 @@
+import { ITicket } from "../types/Model.types";
+
+export const TicketValidator = (form: ITicket) => {
+  const { name, item, phoneNumber } = form;
+
+  return new Promise((resolve, reject) => {
+    if (!form) {
+      reject({ result: null, error: `Empty form` });
+    }
+
+    if (Object.keys(form).length > 3) {
+      reject({ result: null, error: `Invalid form format` });
+    }
+
+    if (!name || name.trim().length === 0) {
+      reject({ result: null, error: `Name not found` });
+    }
+
+    if (!item || item.trim().length === 0) {
+      reject({ result: null, error: `Item not found` });
+    }
+
+    if (!phoneNumber || phoneNumber.trim().length === 0) {
+      reject({ result: null, error: `Phone number not found` });
+    }
+
+    if (!phoneNumber.match(/^(\+234\d{10})|(0\d{10})$/)) {
+      reject({ result: null, error: `Invalid phone number` });
+    }
+
+    resolve("Success");
+  });
+};
