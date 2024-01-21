fb556ab9367b598d27e7a2004adf5103cb7700c0
onfranciis <onukwuf@gmail.com>
2024-01-21 11:29:46 +0100

    fix: update cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import CreatePDF from "./utils/CreatePDF";
 export const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(json());
 app.disable("view cache");
 app.engine("mustache", mustache());
